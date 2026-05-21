1c87c19fd7c308a4c7a06b7005a12e4637aba881
Yilia_Shen <zs427@cam.ac.uk>
2026-05-21 20:11:39 +0100

    part1 code

--- viewer/student_submission/part1_fk.py
+++ viewer/student_submission/part1_fk.py
@@ -22,9 +22,44 @@ def forward_kinematics(
     - world_positions: shape (J, 3)
     """
     joint_count = len(joints)
-    world_rotations = np.tile(
-        np.eye(3, dtype=np.float32)[None, :, :],
-        (joint_count, 1, 1),
-    )
+    
+    # Initialize
     world_positions = np.zeros((joint_count, 3), dtype=np.float32)
+    world_rotations = np.zeros((joint_count, 3, 3), dtype=np.float32)
+
+    # Check if world roataion is already computed
+    computed = [False] * joint_count
+
+    # Ensure parent is calculated before child
+    def parent_before_child_joint(i):
+        if computed[i]:
+            return
+        parent_index = joints[i].parent
+
+        # For root joint
+        if parent_index == -1:
+            # R_world(root) = R_local(root)
+            # p_world(root) = t_root + root_offset
+            world_rotations[i] = local_rotations[i]
+            world_positions[i] = joints[i].translation + root_offset
+
+        # For child joint
+        else:
+            # Calculate its parent joint
+            parent_before_child_joint(parent_index)
+            # R_world(i) = R_world(p(i)) R_local(i)
+            # p_world(i) = p_world(p(i)) + R_world(p(i)) t_local(i)
+            world_rotations[i] = world_rotations[parent_index] @ local_rotations[i]
+            world_positions[i] = world_positions[parent_index] + world_rotations[parent_index] @ joints[i].translation
+        
+        computed[i] = True
+
+    if topological_order is not None:
+        order = topological_order
+    else:
+        order = range(joint_count)
+
+    for i in order:
+        parent_before_child_joint(i)
+    
     return world_rotations, world_positions
